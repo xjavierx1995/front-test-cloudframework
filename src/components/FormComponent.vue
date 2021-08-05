80dbf4d304542131a092adb4ce8247f810946c70
Javier Rodriguez <xjavierx1995@gmail.com>
2021-08-05 16:56:21 -0400

    modal de error

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,154 +1,161 @@
 <template>
-    <v-form>
-        <v-container class="pa-10">
-        <v-row>
-            <v-col cols="12" sm="6" md="3">
-                <v-text-field
-                    name="name"
-                    :error-messages="errors.first('name')"
-                    v-validate="'required'"
-                    label="Nombre"
-                    v-model="form.name"
-                    readonly
-                ></v-text-field>
-            </v-col>
-            <v-col cols="12" sm="6" md="3">
-                <v-text-field
-                    name="surname"
-                    :error-messages="errors.first('surname')"
-                    v-validate="'required'"
-                    label="Apellido"
-                    v-model="form.surname"
-                    readonly
-                ></v-text-field>
-            </v-col>
-            <v-col cols="12" sm="6" md="3">
-                <v-text-field
-                    name="email"
-                    :error-messages="errors.first('email')"
-                    v-validate="'required|email'"
-                    label="Email"
-                    v-model="form.email"
-                    readonly
-                ></v-text-field>
-            </v-col>
-            
-            <v-col cols="12" sm="6" md="3">
-                <v-text-field
-                    name="teléfono"
-                    :error-messages="errors.first('teléfono')"
-                    v-validate="'required|numeric|max:25'"
-                    label="Teléfono"
-                    v-model="form.phone"
-                    type="number"
-                ></v-text-field>
-            </v-col>
+    <div>
+        <v-form>
+            <v-container class="pa-10">
+            <v-row>
+                <v-col cols="12" sm="6" md="3">
+                    <v-text-field
+                        name="name"
+                        :error-messages="errors.first('name')"
+                        v-validate="'required'"
+                        label="Nombre"
+                        v-model="form.name"
+                        readonly
+                    ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="3">
+                    <v-text-field
+                        name="surname"
+                        :error-messages="errors.first('surname')"
+                        v-validate="'required'"
+                        label="Apellido"
+                        v-model="form.surname"
+                        readonly
+                    ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="3">
+                    <v-text-field
+                        name="email"
+                        :error-messages="errors.first('email')"
+                        v-validate="'required|email'"
+                        label="Email"
+                        v-model="form.email"
+                        readonly
+                    ></v-text-field>
+                </v-col>
+                
+                <v-col cols="12" sm="6" md="3">
+                    <v-text-field
+                        name="teléfono"
+                        :error-messages="errors.first('teléfono')"
+                        v-validate="'required|numeric|max:25'"
+                        label="Teléfono"
+                        v-model="form.phone"
+                        type="number"
+                    ></v-text-field>
+                </v-col>
 
-            <v-col cols="12" sm="6" md="3">
-                <v-text-field
-                    name="edad"
-                    :error-messages="errors.first('edad')"
-                    v-validate="'required|numeric|max:3|max_value:100'"
-                    label="Edad"
-                    v-model="form.age"
-                    type="number"
-                ></v-text-field>
-            </v-col>
+                <v-col cols="12" sm="6" md="3">
+                    <v-text-field
+                        name="edad"
+                        :error-messages="errors.first('edad')"
+                        v-validate="'required|numeric|max:3|max_value:100'"
+                        label="Edad"
+                        v-model="form.age"
+                        type="number"
+                    ></v-text-field>
+                </v-col>
 
-            <v-col cols="12" sm="6" md="3">
-                <vuetify-money
-                    v-validate="'required|max_value:1000|min_value:11'"
-                    name="loan_amount"
-                    :error-messages="errors.first('loan_amount')"
-                    v-model="form.loan_amount"
-                    label="Importe préstamo"
-                    :options="{
-                        prefix: '$',
-                        suffix: '',
-                        length: 11,
-                        precision: 0
-                    }"
-                />
-            </v-col>
+                <v-col cols="12" sm="6" md="3">
+                    <vuetify-money
+                        v-validate="'required|max_value:1000|min_value:11'"
+                        name="loan_amount"
+                        :error-messages="errors.first('loan_amount')"
+                        v-model="form.loan_amount"
+                        label="Importe préstamo"
+                        :options="{
+                            prefix: '$',
+                            suffix: '',
+                            length: 11,
+                            precision: 0
+                        }"
+                    />
+                </v-col>
 
-            <v-col cols="12" sm="6" md="3">
-                <v-menu
-                    v-model="loan_date_menu"
-                    :close-on-content-click="false"
-                    :nudge-right="40"
-                    transition="scale-transition"
-                    offset-y
-                    min-width="auto"
-                >
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-text-field
-                            v-model="form.loan_date"
-                            name="fecha"
-                            v-validate="'required|future_date'"
-                            :error-messages="errors.first('fecha')"
-                            label="Fecha del préstamo"
-                            prepend-icon="mdi-calendar"
-                            readonly
-                            v-bind="attrs"
-                            v-on="on"
-                        ></v-text-field>
+                <v-col cols="12" sm="6" md="3">
+                    <v-menu
+                        v-model="loan_date_menu"
+                        :close-on-content-click="false"
+                        :nudge-right="40"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="auto"
+                    >
+                        <template v-slot:activator="{ on, attrs }">
+                            <v-text-field
+                                v-model="form.loan_date"
+                                name="fecha"
+                                v-validate="'required|future_date'"
+                                :error-messages="errors.first('fecha')"
+                                label="Fecha del préstamo"
+                                prepend-icon="mdi-calendar"
+                                readonly
+                                v-bind="attrs"
+                                v-on="on"
+                            ></v-text-field>
+                        </template>
+                        <v-date-picker 
+                            :allowed-dates="allowedDates" 
+                            v-model="form.loan_date" 
+                            @input="loan_date_menu = false"
+                        ></v-date-picker>
+                    </v-menu>
+                </v-col>
+
+                <v-col cols="12" sm="6" md="3">
+                    <v-select
+                        name="semanas"
+                        :error-messages="errors.first('semanas')"
+                        v-validate="'required'"
+                        label="Tiempo a devolver"
+                        v-model="form.loan_weeks"
+                        :items="weeks"
+                        item-text="name"
+                        item-value="value"
+                    ></v-select>
+                </v-col>
+
+                <v-col cols="12" sm="6" md="3">
+                    <v-checkbox
+                        name="terminos"
+                        :error-messages="errors.first('terminos')"
+                        v-validate="'required|required_true'"
+                        label="Aceptar térnimos y condiciones"
+                        v-model="form.check"
+                    >
+                        <template v-slot:label>
+                    <div>
+                            <a
+                            target="_blank"
+                            href="https://comparte.bnext.io/privacidad"
+                            @click.stop
+                            >
+                            <u>Términos y condiciones</u>
+                            </a>
+                    </div>
                     </template>
-                    <v-date-picker 
-                        :allowed-dates="allowedDates" 
-                        v-model="form.loan_date" 
-                        @input="loan_date_menu = false"
-                    ></v-date-picker>
-                </v-menu>
-            </v-col>
+                    </v-checkbox>
+                </v-col>
 
-            <v-col cols="12" sm="6" md="3">
-                <v-select
-                    name="semanas"
-                    :error-messages="errors.first('semanas')"
-                    v-validate="'required'"
-                    label="Tiempo a devolver"
-                    v-model="form.loan_weeks"
-                    :items="weeks"
-                    item-text="name"
-                    item-value="value"
-                ></v-select>
-            </v-col>
+                <v-col cols="12" sm="12" md="12" class="text-right">
+                    <v-spacer></v-spacer>
+                    <v-btn @click="submit" :loading="loading" elevation="0" color="purple" class="white--text">
+                        Guardar
+                    </v-btn>
+                </v-col>
 
-            <v-col cols="12" sm="6" md="3">
-                <v-checkbox
-                    name="terminos"
-                    :error-messages="errors.first('terminos')"
-                    v-validate="'required|required_true'"
-                    label="Aceptar térnimos y condiciones"
-                    v-model="form.check"
-                >
-                    <template v-slot:label>
-                  <div>
-                        <a
-                          target="_blank"
-                          href="https://comparte.bnext.io/privacidad"
-                          @click.stop
-                        >
-                          <u>Términos y condiciones</u>
-                        </a>
-                  </div>
-                </template>
-                </v-checkbox>
-            </v-col>
-
-            <v-col cols="12" sm="12" md="12" class="text-right">
-                <v-spacer></v-spacer>
-                <v-btn @click="submit" :loading="loading" elevation="0" color="purple" class="white--text">
-                    Guardar
-                </v-btn>
-            </v-col>
-
-        </v-row>
-        </v-container>
-    </v-form>
+            </v-row>
+            </v-container>
+        </v-form>
+        <ModalErrorComponent :showDialog="showErrorDialog"/>
+    </div>
 </template>
 <script>
+import ModalErrorComponent from './ModalError.vue'
 export default {
+    components: {
+        ModalErrorComponent
+    },
     data() {
         return {
             user: {},
@@ -171,7 +178,8 @@ export default {
                     name: '1 semana'
                 }
             ],
-            loading: false
+            loading: false,
+            showErrorDialog: false
         }
     },
     mounted() {
@@ -180,7 +188,13 @@ export default {
         }
         this.$axios.get('users', { params }).then(response => {
             this.user = response.data.data;
+            this.showErrorDialog = false;
             this.initForm();
+        }).catch(err => {
+            if (err.response.data.status == 400 || err.response.data.status == 404) {
+                console.log('error');
+                this.showErrorDialog = true;
+            }
         })
     },
     methods: {
---
+++ src/components/ModalError.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="text-center">
+    <v-dialog
+      v-model="showDialog"
+      persistent
+      max-width="350"
+    >
+      <v-card>
+        <v-card-title class="text-h5">
+          Usuario no encontrado
+        </v-card-title>
+        <v-card-text class="black--text">
+            No existe un usuario con el numero de identificación <span class="text-weight-bold">{{$route.params.id }}</span>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+<script>
+  export default {
+    props: [
+        'showDialog',
+        'user_id'
+    ],
+    data () {
+      return {
+        dialog: false,
+      }
+    },
+  }
+</script>
