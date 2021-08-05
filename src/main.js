6afd59b8a3946219755b5d086b2f376039a71f5e
Javier Rodriguez <xjavierx1995@gmail.com>
2021-08-05 01:10:53 -0400

    vee-validate

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import VeeValidate from 'vee-validate';
+
+Vue.use(VeeValidate);
 
 Vue.config.productionTip = false
 
