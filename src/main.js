aba24eff309da7bac91bd4b277992929324dedd2
Javier Rodriguez <xjavierx1995@gmail.com>
2021-08-05 01:14:50 -0400

    composition api

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import VeeValidate from 'vee-validate';
+import VueCompositionAPI from '@vue/composition-api'
 
+Vue.use(VueCompositionAPI)
 Vue.use(VeeValidate);
 
 Vue.config.productionTip = false
