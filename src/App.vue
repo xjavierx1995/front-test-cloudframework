fc275752da81699fff4dcebf705c07482154c4d0
Javier Rodriguez <xjavierx1995@gmail.com>
2021-08-07 17:52:28 -0400

    barra de navegacion

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@
       app
       color="purple"
       dark
+      class="text-center"
     >
       <div class="d-flex align-center">
-
-        <router-link :to="'/home/'+$route.params.id" class="text-decoration-none text-md-h6 white--text text-uppercase">
+        <router-link :to="'/'+$route.params.id" class="text-decoration-none text-md-h6 white--text text-uppercase">
           cloud framework 
         </router-link>
       </div>
@@ -15,6 +15,7 @@
       <v-spacer></v-spacer>
 
       <v-btn
+        class="d-sm-flex d-none"
         :to="'/form/'+$route.params.id"
         text
       >
@@ -25,6 +26,20 @@
     <v-main>
       <router-view/>
     </v-main>
+
+    <v-bottom-navigation class="d-sm-none d-flex" app fixed dark background-color="purple">
+      <v-btn :to="'/'+$route.params.id" class="white--text">
+        <span>Inicio</span>
+
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+
+      <v-btn :to="'/form/'+$route.params.id">
+        <span>Prestamo</span>
+
+        <v-icon>mdi-cash</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </v-app>
 </template>
 
